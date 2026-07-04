034b7d91ccb6181a0c161349e5f014d8a31ca7cb
yuan he <yuan.he.uw@login1.cloud.r-ccs.riken.jp>
2026-07-04 15:59:22 +0900

    PIMID 1.5.9: README spacing between intro/QR and schematic

--- src/main.cpp
+++ src/main.cpp
@@ -5374,7 +5374,7 @@ void printUsage(const char* program_name) {
 
 void printVersion() {
     std::cout << "PIMID - Processing-In-Memory Infrastructure for Design-space exploration" << std::endl;
-    std::cout << "Version 1.5.8" << std::endl;
+    std::cout << "Version 1.5.9" << std::endl;
     std::cout << std::endl;
     std::cout << "Integrated External Models:" << std::endl;
 #ifdef HAVE_RAMULATOR
